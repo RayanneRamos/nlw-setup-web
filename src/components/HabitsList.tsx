0cb3638347c6b2d38dafd8301f09af59d6f8627e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-01-21 12:18:06 -0300

    sincronizando hábitos completos

--- src/components/HabitsList.tsx
+++ src/components/HabitsList.tsx
@@ -6,6 +6,7 @@ import { api } from '../lib/axios';
 
 interface HabitsListProps {
   date: Date;
+  onCompletedChanged: (completed: number) => void;
 }
 
 interface HabitsInfo {
@@ -14,12 +15,11 @@ interface HabitsInfo {
     title: string;
     created_at: string;
   }>,
-  completedHabits: string;
+  completedHabits: string[];
 }
 
-export function HabitsList({ date }: HabitsListProps) {
+export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
   const [ habitsInfo, setHabitsInfo ] = useState<HabitsInfo>();
-  const isDateInPast = dayjs(date).endOf('day').isBefore(new Date());
   
   useEffect(() => {
     api.get('day', {
@@ -31,14 +31,39 @@ export function HabitsList({ date }: HabitsListProps) {
     })
   }, []);
 
+  async function handleToggleHabit(habitId: string) {
+    const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId)
+  
+    await api.patch(`/habits/${habitId}/toggle`)
+
+    let completedHabits: string[] = []
+
+    if(isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId)
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId]
+    }
+
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits
+    });
+
+    onCompletedChanged(completedHabits.length);
+  }
+
+  const isDateInPast = dayjs(date).endOf('day').isBefore(new Date());
+
   return (
     <div className='mt-6 flex flex-col gap-3'>
       { habitsInfo?.possibleHabits.map((habit) => {
         return (
           <Checkbox.Root 
-            className='flex items-center gap-3 group' 
             key={habit.id}
+            onCheckedChange={() => handleToggleHabit(habit.id)}
             checked={habitsInfo.completedHabits.includes(habit.id)}
+            disabled={isDateInPast}
+            className='flex items-center gap-3 group' 
           >
             <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
               <Checkbox.Indicator>
