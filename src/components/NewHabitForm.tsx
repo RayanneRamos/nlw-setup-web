6ffb2594f90db4fd9121b194175d150a53f33dd0
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-01-21 12:38:39 -0300

    adicionando as transições ao projeto

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -49,7 +49,7 @@ export function NewHabitForm() {
         id='title'
         placeholder='ex: Exercícios, dormir bem, etc...'
         autoFocus
-        className='p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400'
+        className='p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900'
         value={title}
         onChange={(event) => setTitle(event.target.value)}
       />
@@ -60,12 +60,12 @@ export function NewHabitForm() {
         { availableWeekDays.map((weekDay, index) => {
           return (
             <Checkbox.Root 
-              className='flex items-center gap-3 group' 
+              className='flex items-center gap-3 group focus:outline-none' 
               checked={weekDays.includes(index)}
               key={weekDay}
               onCheckedChange={() => handleToggleWeekDay(index)}
             >
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:outline-none group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background'>
                 <Checkbox.Indicator>
                   <Check size={20} className='text-white' />
                 </Checkbox.Indicator>
@@ -78,7 +78,10 @@ export function NewHabitForm() {
         }) }
         
       </div>
-      <button type='submit' className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
+      <button 
+        type='submit' 
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
+      >
         <Check size={20} weight='bold' />
         Confirmar
       </button>
