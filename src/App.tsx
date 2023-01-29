81091d26455cc48388cdc01dffee822d172ff4e5
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-01-28 22:13:00 -0300

    fazendo as chamadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
-import './styles/global.css';
-import logoImage from './assets/logo.svg';
-import { Plus } from 'phosphor-react';
-import { Header } from './components/Header';
-import { SummaryTable } from './components/SummaryTable';
-import './lib/dayjs';
+import "./styles/global.css";
+import logoImage from "./assets/logo.svg";
+import { Plus } from "phosphor-react";
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
+import "./lib/dayjs";
+import { api } from "./lib/axios";
+
+navigator.serviceWorker
+  .register("service-worker.js")
+  .then(async (serviceWorker) => {
+    let subscription = await serviceWorker.pushManager.getSubscription();
+
+    if (!subscription) {
+      const publicKeyResponse = await api.get("/push/public_key");
+
+      subscription = await serviceWorker.pushManager.subscribe({
+        applicationServerKey: publicKeyResponse.data.publicKey,
+        userVisibleOnly: true,
+      });
+    }
+
+    await api.post("/push/register", {
+      subscription,
+    });
+
+    await api.post("/push/send", {
+      subscription,
+    });
+  });
 
 export function App() {
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
         <SummaryTable />
       </div>
     </div>
   );
 }
-
